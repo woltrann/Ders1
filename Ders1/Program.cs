167013c4924c03e58c248ff2503f4fa6c3afb137
woltrann <harunelaltunkara@gmail.com>
2022-02-08 07:08:48 +0300

    yenilendi

--- Ders1/Program.cs
+++ Ders1/Program.cs
@@ -7,6 +7,7 @@ namespace Ders1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
