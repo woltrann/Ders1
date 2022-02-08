7555cb6c22926761c9efbd09e6f7592c9b641cd3
woltrann <harunelaltunkara@gmail.com>
2022-02-08 07:15:31 +0300

    yeni

--- Ders1/Program.cs
+++ Ders1/Program.cs
@@ -6,8 +6,15 @@ namespace Ders1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Stranger!");
+            Console.WriteLine("What is your name?");
+            string x = Console.ReadLine();
+            Console.WriteLine("What is your surname?");
+            string y = Console.ReadLine(y);
+            Console.WriteLine("Okay let's start" + x + " " + y);
+
+
+
         }
     }
 }
